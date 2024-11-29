921cc6dd3e9a8ce7e3e1aedbeb211322c52bf87b
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-11-29 18:24:02 +0800

    append first version

--- linked.js
+++ linked.js
@@ -2,12 +2,26 @@ class Linkedlist {
   constructor() {
     this.head = new Node(null, null);
   }
-
-  append(item) {}
+  getList() {
+    return this.head;
+  }
+  append(item) {
+    const node = new Node(item);
+    let temp = this.head;
+    if (!this.head.val) {
+      this.head = node;
+    } else {
+      while (temp !== null) {
+        temp = temp.next;
+      }
+      temp = node;
+    }
+    this.head.next = temp;
+  }
 }
 
 class Node {
-  constructor(val, next) {
+  constructor(val = null, next = null) {
     this.val = val;
     this.next = next;
   }
@@ -16,4 +30,4 @@ class Node {
 const list = new Linkedlist();
 list.append("dog");
 list.append("cat");
-console.log(list.head.next);
+console.log(list.getList());
