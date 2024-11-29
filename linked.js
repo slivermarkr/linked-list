9447a875f2f789637c3d3b320b5ed8a079807bf1
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-11-29 18:39:27 +0800

    addFirst method

--- linked.js
+++ linked.js
@@ -1,24 +1,29 @@
 class Linkedlist {
   constructor() {
-    this.head = new Node(null, null);
+    this.head = undefined;
   }
+
   getList() {
     return this.head;
   }
+
+  addFirst(item) {
+    if (!this.head) {
+      this.head = new Node(item);
+    }
+  }
+
   append(item) {
-    const node = new Node(item);
     let temp = this.head;
 
-    if (!this.head.val) {
-      //create and entry
-      this.head = node;
+    if (!this.head) {
+      this.addFirst(item);
     } else {
       while (temp.next !== null) {
         temp = temp.next;
       }
-      temp.next = node;
+      temp.next = new Node(item);
     }
-    // this.head.next = temp;
   }
 }
 
@@ -33,4 +38,6 @@ const list = new Linkedlist();
 list.append("dog");
 list.append("cat");
 list.append("fish");
+list.append("bird");
 console.log(list.getList());
+console.log(list.getList().next.next.next);
